f9e831fe7ffc3a9ac38d8286e8d425937a5219d5
Theodore Harlan <hpt@bu.edu>
2026-09-18 05:42:12 -0500

    rpc installer

---
+++ Formula/booksrpc.rb
@@ -0,0 +1,35 @@
+class Booksrpc < Formula
+  desc "Discord rich presence for Apple Books"
+  homepage "https://github.com/sqrt9/BooksRPC"
+  version "0.1.1"
+  license "MIT"
+
+  on_macos do 
+    on_arm64 do
+      url "https://github.com/sqrt9/BooksRPC/releases/download/v0.1.1/booksrpc-aarch64"
+      sha256 "1cff486484366d9fbc3ed8d24801c05a80292a00b8d0453668aabbe9b3e2ee3c"
+    end
+    on_intel do
+      url "https://github.com/sqrt9/BooksRPC/releases/download/v0.1.1/booksrpc-x86_64"
+      sha256 "d6bfed6b3725030f706baef8158c3d0fdfea8660b4a1ac61d077b9d84c0e1d09"
+    end
+  end
+
+  def install
+    if Hardware::CPU.arm?
+      bin.install "booksrpc-aarch64" => "booksrpc"
+    else
+      bin.install "booksrpc-x86_64" => "booksrpc"
+    end
+  end
+
+  service do
+    run opt_bin/"booksrpc"
+    keep_alive true
+  end
+
+  test do
+    system "#{bin}/booksrpc", "--version"
+  end
+  
+end
