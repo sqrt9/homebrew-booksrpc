f927d637aa9ae3a98d3c04bc0e7aabf44146cac4
Theodore Harlan <hpt@bu.edu>
2026-09-18 05:49:33 -0500

    correct some syntax

--- Formula/booksrpc.rb
+++ Formula/booksrpc.rb
@@ -4,15 +4,15 @@ class Booksrpc < Formula
   version "0.1.1"
   license "MIT"
 
-  on_arm64 do
-    url "https://github.com/sqrt9/BooksRPC/releases/download/v0.1.1/booksrpc-aarch64"
-    sha256 "1cff486484366d9fbc3ed8d24801c05a80292a00b8d0453668aabbe9b3e2ee3c"
-  end
-
-  on_intel do
-    url "https://github.com/sqrt9/BooksRPC/releases/download/v0.1.1/booksrpc-x86_64"
-    sha256 "d6bfed6b3725030f706baef8158c3d0fdfea8660b4a1ac61d077b9d84c0e1d09"
-  end
+  if OS.mac?
+    if Hardware::CPU.arm? do
+      url "https://github.com/sqrt9/BooksRPC/releases/download/v0.1.1/booksrpc-aarch64"
+      sha256 "1cff486484366d9fbc3ed8d24801c05a80292a00b8d0453668aabbe9b3e2ee3c"
+    end
+    if Hardware::CPU.intel? do
+      url "https://github.com/sqrt9/BooksRPC/releases/download/v0.1.1/booksrpc-x86_64"
+      sha256 "d6bfed6b3725030f706baef8158c3d0fdfea8660b4a1ac61d077b9d84c0e1d09"
+    end
 
   def install
     if Hardware::CPU.arm?
