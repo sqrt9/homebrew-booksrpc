49da6589959d723d9fd0aaf9b5949a21549aaa42
Theodore Harlan <hpt@bu.edu>
2026-09-18 05:54:00 -0500

    Passed the audit linter

--- Formula/booksrpc.rb
+++ Formula/booksrpc.rb
@@ -1,18 +1,18 @@
 class Booksrpc < Formula
   desc "Discord rich presence for Apple Books"
   homepage "https://github.com/sqrt9/BooksRPC"
-  version "0.1.1"
   license "MIT"
 
   if OS.mac?
-    if Hardware::CPU.arm? do
+    if Hardware::CPU.arm?
       url "https://github.com/sqrt9/BooksRPC/releases/download/v0.1.1/booksrpc-aarch64"
       sha256 "1cff486484366d9fbc3ed8d24801c05a80292a00b8d0453668aabbe9b3e2ee3c"
     end
-    if Hardware::CPU.intel? do
+    if Hardware::CPU.intel?
       url "https://github.com/sqrt9/BooksRPC/releases/download/v0.1.1/booksrpc-x86_64"
       sha256 "d6bfed6b3725030f706baef8158c3d0fdfea8660b4a1ac61d077b9d84c0e1d09"
     end
+  end
 
   def install
     if Hardware::CPU.arm?
@@ -30,5 +30,4 @@ class Booksrpc < Formula
   test do
     system "#{bin}/booksrpc", "--version"
   end
-
 end
